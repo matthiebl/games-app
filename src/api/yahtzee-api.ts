6f6058cbefd4d1ca82bccb13f1c0ab3c350fae37
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-08-30 08:44:18 +1000

    Implement some rough layout

--- src/api/yahtzee-api.ts
+++ src/api/yahtzee-api.ts
@@ -1,10 +1,14 @@
-import { addDoc, collection, doc, onSnapshot } from 'firebase/firestore'
+import { addDoc, collection, doc, onSnapshot, runTransaction, updateDoc } from 'firebase/firestore'
 import { GameID, UserID, addGame, database } from '.'
 
 export type YahtzeeGame = {
     players: YahtzeePlayer[]
     playersJoined: boolean
-    turn: number
+    turn: {
+        playerIndex: number
+        roll: number
+        dice: number[]
+    }
     winner: number
 }
 
@@ -16,45 +20,47 @@ export type YahtzeePlayer = {
 
 export type CardValue = number | null
 
-export type YahtzeeCard = {
-    ones: CardValue
-    twos: CardValue
-    threes: CardValue
-    fours: CardValue
-    fives: CardValue
-    sixes: CardValue
-    bonus: CardValue
-    triple: CardValue
-    quadruple: CardValue
-    fullHouse: CardValue
-    smallStraight: CardValue
-    largeStraight: CardValue
-    yahtzee: CardValue
-    extras: CardValue
-    chance: CardValue
-}
+export type YahtzeeCard = CardValue[]
+// {
+//     ones: CardValue
+//     twos: CardValue
+//     threes: CardValue
+//     fours: CardValue
+//     fives: CardValue
+//     sixes: CardValue
+//     bonus: CardValue
+//     triple: CardValue
+//     quadruple: CardValue
+//     fullHouse: CardValue
+//     smallStraight: CardValue
+//     largeStraight: CardValue
+//     yahtzee: CardValue
+//     extras: CardValue
+//     chance: CardValue
+// }
 
 const newPlayer = (uid: UserID, name: string): YahtzeePlayer => {
     return {
         uid,
         name,
-        card: {
-            ones: null,
-            twos: null,
-            threes: null,
-            fours: null,
-            fives: null,
-            sixes: null,
-            bonus: null,
-            triple: null,
-            quadruple: null,
-            fullHouse: null,
-            smallStraight: null,
-            largeStraight: null,
-            yahtzee: null,
-            extras: null,
-            chance: null,
-        },
+        card: [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        // {
+        //     ones: null,
+        //     twos: null,
+        //     threes: null,
+        //     fours: null,
+        //     fives: null,
+        //     sixes: null,
+        //     bonus: null,
+        //     triple: null,
+        //     quadruple: null,
+        //     fullHouse: null,
+        //     smallStraight: null,
+        //     largeStraight: null,
+        //     yahtzee: null,
+        //     extras: null,
+        //     chance: null,
+        // },
     }
 }
 
@@ -62,7 +68,11 @@ export const createYahtzeeGame = (uid: UserID, name: string, callback: (id: Game
     const newGame: YahtzeeGame = {
         players: [newPlayer(uid, name)],
         playersJoined: false,
-        turn: 0,
+        turn: {
+            playerIndex: -1,
+            roll: 0,
+            dice: [1, 1, 1, 1, 1],
+        },
         winner: -1,
     }
 
@@ -80,7 +90,7 @@ export const createYahtzeeGame = (uid: UserID, name: string, callback: (id: Game
 }
 
 export const getYahtzeeGame = (gid: GameID, callback: (game: YahtzeeGame) => any) => {
-    onSnapshot(doc(database, 'connect', gid), doc => {
+    onSnapshot(doc(database, 'yahtzee', gid), doc => {
         if (doc.exists()) {
             const data = doc.data()
             callback({
@@ -92,5 +102,34 @@ export const getYahtzeeGame = (gid: GameID, callback: (game: YahtzeeGame) => any
         } else {
             console.warn('[YAHTZEE] No game of yahtzee with id', gid)
         }
+    })
+}
+
+export const putYahtzeePlayer = (gid: GameID, uid: UserID, name: string) => {
+    const gameRef = doc(database, 'yahtzee', gid)
+
+    try {
+        runTransaction(database, async transaction => {
+            const doc = await transaction.get(gameRef)
+            if (doc.exists()) {
+                const data = doc.data()
+                const find = data.players.filter((player: YahtzeePlayer) => player.uid === uid)
+                if (!data.playersJoined && find.length === 0) {
+                    transaction.update(gameRef, { players: [...data.players, newPlayer(uid, name)] })
+                }
+            }
+        })
+    } catch (e) {
+        console.error('[YAHTZEE] Adding player to game failed', e)
+    }
+}
+
+export const putYahtzeeAllPlayersJoined = (gid: GameID) => {
+    updateDoc(doc(database, 'yahtzee', gid), {
+        playersJoined: true,
+    }).catch(error => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.error('[YAHTZEE] Setting playersJoined failed with code', errorCode, errorMessage)
     })
 }
--- src/pages/YahtzeePlay.tsx
+++ src/pages/YahtzeePlay.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import {
-    ConnectGame,
-    ConnectPiece,
     GameID,
+    UserData,
     UserID,
     YahtzeeGame,
-    connectGameMessage,
-    createConnectGame,
     getYahtzeeGame,
+    putYahtzeeAllPlayersJoined,
     sendGameInvite,
 } from '../api'
-import { Button, Page, classNames } from '../components'
+import { Button, Page } from '../components'
 import { UserContext } from '../context'
-
-type Die = 1 | 2 | 3 | 4 | 5 | 6 | '?'
 
 interface YahtzeePlayProps {}
 
@@ -26,7 +22,7 @@ export const YahtzeePlay: React.FC<YahtzeePlayProps> = ({}) => {
     const [playerIndex, setPlayerIndex] = React.useState<number>(-1)
     const [game, setGame] = React.useState<YahtzeeGame | null>(null)
 
-    const [dice, setDice] = React.useState<Die[]>(['?', '?', '?', '?', '?'])
+    const [dice, setDice] = React.useState<Die[]>(gameDiceToPlayDice([1, 1, 1, 1, 1]))
     const [shareMessage, setShareMessage] = React.useState<string>('Share Game')
 
     React.useEffect(() => {
@@ -37,6 +33,7 @@ export const YahtzeePlay: React.FC<YahtzeePlayProps> = ({}) => {
         }
         getYahtzeeGame(gid, data => {
             setGame(data)
+            setDice(gameDiceToPlayDice(data.turn.dice))
             setPlayerIndex(getPlayerIndex(data, user.uid))
         })
     }, [user, gid])
@@ -64,180 +61,263 @@ export const YahtzeePlay: React.FC<YahtzeePlayProps> = ({}) => {
     }
 
     const makeTurn = () => {
-        if (user === null || game === null || gid === undefined || playerIndex !== game.turn) {
+        if (user === null || game === null || gid === undefined || playerIndex !== game.turn.playerIndex) {
             return
         }
     }
-
-    const GameBoard = game === null ? EmptyBoard : Board
 
     return (
         <Page centered>
             <div className='w-full max-w-4xl p-5 gap-5 flex flex-col items-center'>
                 <h1 className='text-3xl font-extrabold text-center'>Yahtzee</h1>
                 <div className='w-full gap-5 flex flex-col items-center'>
-                    <div className='w-full p-8'>
-                        {dice.map(die => (
-                            <button className='w-10 h-10 flex justify-center items-center'>{die}</button>
-                        ))}
-                    </div>
-                    {/* <div className='relative max-w-xl w-full h-full'>
-                        <div
-                            data-finish={game?.winner !== ''}
-                            className='hidden z-40 absolute w-full h-full data-[finish=true]:flex items-center justify-center text-4xl font-extrabold'
-                        >
-                            {game !== null && connectGameMessage(game, player)}
+                    <PlayersSection gid={gid} game={game} user={user} />
+                    <Dice game={game} dice={dice} />
+
+                    <h3 className='text-xl w-full -mb-3 text-left'>Scorecard</h3>
+                    <div className='w-full flex'>
+                        <div className=''>
+                            {[
+                                'Aces',
+                                'Twos',
+                                'Threes',
+                                'Fours',
+                                'Fives',
+                                'Sixes',
+                                'Total',
+                                'Bonus',
+                                'Upper Total',
+                                '3 of a Kind',
+                                '4 of a Kind',
+                                'Full House',
+                                'SM Straight',
+                                'LG Straight',
+                                'YAHTZEE',
+                                'Chance',
+                                'BONUS',
+                                'Lower Total',
+                                'Grand Total',
+                            ].map(s => (
+                                <div>{s}</div>
+                            ))}
                         </div>
-                        <GameBoard player={player} game={game} placePiece={placePiece} />
+                        <div className='px-2 py-1 text-center'>
+                            {game !== null && user !== null && game.players[getPlayerIndex(game, user.uid)].card}
+                        </div>
                     </div>
-                    <InfoSection
-                        gid={gid}
-                        game={game}
-                        player={player}
-                        shareMessage={shareMessage}
-                        shareGame={shareGame}
-                    /> */}
                 </div>
             </div>
         </Page>
     )
 }
 
-interface BoardProps {
-    player: ConnectPiece | ''
-    game: ConnectGame | null
-    placePiece: (column: number) => void
-}
-
-const Board: React.FC<BoardProps> = ({ player, game, placePiece }) => {
-    if (game === null) {
-        return <EmptyBoard player={player} game={game} placePiece={placePiece} />
-    }
-
-    return (
-        <div className='w-full aspect-[7/6] grid items-end grid-cols-7 gap-2'>
-            {game.board.map((column, columnIndex) => (
-                <button
-                    key={crypto.randomUUID()}
-                    data-player={player}
-                    disabled={
-                        game === null ||
-                        game.turn !== player ||
-                        game.player2 === '' ||
-                        game.winner !== '' ||
-                        game.board[columnIndex].length >= 6
-                    }
-                    onClick={() => placePiece(columnIndex)}
-                    className='rounded-full border h-full w-full flex flex-col-reverse gap-2.5 disabled:border-gray-200 data-[player="1"]:border-red-500 data-[player="2"]:border-yellow-500 disabled:data-[player="1"]:border-gray-200 disabled:data-[player="2"]:border-gray-200'
-                >
-                    {column.split('').map(piece => (
-                        <div
-                            key={crypto.randomUUID()}
-                            className={classNames(
-                                'w-full aspect-square rounded-full',
-                                piece === '1' ? 'bg-red-500' : 'bg-yellow-500',
-                                columnIndex === game.lastColumn
-                                    ? 'last:animate-fall last:motion-safe:animate-fall last:motion-reduce:animate-none'
-                                    : '',
-                            )}
-                        />
-                    ))}
-                </button>
-            ))}
-        </div>
-    )
-}
-
-const EmptyBoard: React.FC<BoardProps> = () => (
-    <div className='w-full aspect-[7/6] grid items-end grid-cols-7 gap-2'>
-        {['', '', '', '', '', '', ''].map(() => (
-            <button
-                key={crypto.randomUUID()}
-                disabled={true}
-                className='rounded-full border h-full w-full flex flex-col-reverse gap-3 disabled:border-gray-200'
-            ></button>
-        ))}
-    </div>
-)
-
-const redPlayer = (player: ConnectPiece | ''): string => {
-    if (player === '') {
-        return 'Player 1'
-    } else if (player === '1') {
-        return 'You'
-    } else {
-        return 'Opponent'
-    }
-}
-
-const yellowPlayer = (player: ConnectPiece | ''): string => {
-    if (player === '') {
-        return 'Player 2'
-    } else if (player === '2') {
-        return 'You'
-    } else {
-        return 'Opponent'
-    }
-}
-
-interface InfoSectionProps {
+interface PlayersSectionProps {
     gid: GameID | undefined
-    game: ConnectGame | null
-    player: ConnectPiece | ''
-    shareMessage: string
-    shareGame: () => void
+    game: YahtzeeGame | null
+    user: UserData | null
 }
 
-const InfoSection: React.FC<InfoSectionProps> = ({ gid, game, player, shareMessage, shareGame }) => {
-    const navigate = useNavigate()
-
+const PlayersSection: React.FC<PlayersSectionProps> = ({ gid, game, user }) => {
     return (
-        <div className='flex flex-col max-w-xl w-full gap-5'>
-            <div className='py-2 px-3 gap-3 shadow rounded-md grid grid-cols-2'>
-                <div className=''>
-                    <p className='text-sm text-red-500'>{redPlayer(player)}</p>
-                    <p>{game?.player1Name || 'Loading'}</p>
-                </div>
-                <div className='text-right'>
-                    <p className='text-sm text-yellow-500'>{yellowPlayer(player)}</p>
-                    <p>{game?.player2Name || 'Loading'}</p>
-                </div>
-                <div className='col-span-2 text-center font-bold'>
-                    {game === null ? 'LOADING' : connectGameMessage(game, player)}
-                </div>
+        <>
+            <div className='w-full flex gap-2 justify-center'>
+                <span>{game === null || game.playersJoined ? 'Players' : 'Players Joined'}:</span>
+                {game === null ? (
+                    <span>Loading Players</span>
+                ) : (
+                    game.players.map((player, i) => (
+                        <React.Fragment key={crypto.randomUUID()}>
+                            <span
+                                data-user={user !== null && player.uid === user.uid}
+                                data-turn={i === game.turn.playerIndex}
+                                className='data-[user=true]:underline data-[turn=true]:font-bold'
+                            >
+                                {user !== null && player.uid === user.uid ? 'You' : player.name}
+                                {i === 0 && ' (Host)'}
+                            </span>
+                            <span className='last:hidden'>•</span>
+                        </React.Fragment>
+                    ))
+                )}
             </div>
-            {game !== null && game.winner !== '' && player !== '' && (
+            {game !== null && user !== null && !game.playersJoined && getPlayerIndex(game, user.uid) === 0 && (
                 <Button
                     onClick={() => {
-                        if (gid === undefined || game === null) {
-                            return
-                        }
-                        const uid = player === '1' ? game.player1 : game.player2
-                        const name = player === '1' ? game.player1Name : game.player2Name
-                        const opponent = player === '1' ? game.player2 : game.player1
-                        createConnectGame(uid, name, id => {
-                            sendGameInvite(opponent, 'connect', id)
-                            navigate(`/play/connect/${id}`)
-                        })
+                        if (gid !== undefined) putYahtzeeAllPlayersJoined(gid)
                     }}
-                    className='bg-sky-500 text-white'
+                    className='w-full bg-lime-500 text-white'
                 >
-                    Rematch Opponent
+                    Start Game
                 </Button>
             )}
-            <Button
-                onClick={shareGame}
-                disabled={game === null || game.winner !== ''}
-                className='bg-sky-500 text-white disabled:bg-gray-400'
-            >
-                {shareMessage}
-            </Button>
-            <span className='-mt-3 text-sm text-gray-600'>
-                Invite code: {window.location.pathname.split('/').at(-1)}
-            </span>
-        </div>
+        </>
     )
 }
+
+interface DieProps {
+    game: YahtzeeGame | null
+    dice: Die[]
+}
+
+const Dice: React.FC<DieProps> = ({ game, dice }) => {
+    return (
+        <>
+            <div className='w-full py-2 px-5 flex justify-center items-center flex-wrap gap-5'>
+                {dice.map(die => (
+                    <button
+                        key={crypto.randomUUID()}
+                        className='w-16 h-16 min-w-[4rem] min-h-[4rem] flex justify-center items-center border rounded border-black'
+                    >
+                        {die.value}
+                    </button>
+                ))}
+            </div>
+            <Button
+                disabled={game === null || !game.playersJoined}
+                onClick={() => {}}
+                className='w-full bg-sky-500 text-white'
+            >
+                Roll
+            </Button>
+        </>
+    )
+}
+
+// interface BoardProps {
+//     player: ConnectPiece | ''
+//     game: ConnectGame | null
+//     placePiece: (column: number) => void
+// }
+
+// const Board: React.FC<BoardProps> = ({ player, game, placePiece }) => {
+//     if (game === null) {
+//         return <EmptyBoard player={player} game={game} placePiece={placePiece} />
+//     }
+
+//     return (
+//         <div className='w-full aspect-[7/6] grid items-end grid-cols-7 gap-2'>
+//             {game.board.map((column, columnIndex) => (
+//                 <button
+//                     key={crypto.randomUUID()}
+//                     data-player={player}
+//                     disabled={
+//                         game === null ||
+//                         game.turn !== player ||
+//                         game.player2 === '' ||
+//                         game.winner !== '' ||
+//                         game.board[columnIndex].length >= 6
+//                     }
+//                     onClick={() => placePiece(columnIndex)}
+//                     className='rounded-full border h-full w-full flex flex-col-reverse gap-2.5 disabled:border-gray-200 data-[player="1"]:border-red-500 data-[player="2"]:border-yellow-500 disabled:data-[player="1"]:border-gray-200 disabled:data-[player="2"]:border-gray-200'
+//                 >
+//                     {column.split('').map(piece => (
+//                         <div
+//                             key={crypto.randomUUID()}
+//                             className={classNames(
+//                                 'w-full aspect-square rounded-full',
+//                                 piece === '1' ? 'bg-red-500' : 'bg-yellow-500',
+//                                 columnIndex === game.lastColumn
+//                                     ? 'last:animate-fall last:motion-safe:animate-fall last:motion-reduce:animate-none'
+//                                     : '',
+//                             )}
+//                         />
+//                     ))}
+//                 </button>
+//             ))}
+//         </div>
+//     )
+// }
+
+// const EmptyBoard: React.FC<BoardProps> = () => (
+//     <div className='w-full aspect-[7/6] grid items-end grid-cols-7 gap-2'>
+//         {['', '', '', '', '', '', ''].map(() => (
+//             <button
+//                 key={crypto.randomUUID()}
+//                 disabled={true}
+//                 className='rounded-full border h-full w-full flex flex-col-reverse gap-3 disabled:border-gray-200'
+//             ></button>
+//         ))}
+//     </div>
+// )
+
+// const redPlayer = (player: ConnectPiece | ''): string => {
+//     if (player === '') {
+//         return 'Player 1'
+//     } else if (player === '1') {
+//         return 'You'
+//     } else {
+//         return 'Opponent'
+//     }
+// }
+
+// const yellowPlayer = (player: ConnectPiece | ''): string => {
+//     if (player === '') {
+//         return 'Player 2'
+//     } else if (player === '2') {
+//         return 'You'
+//     } else {
+//         return 'Opponent'
+//     }
+// }
+
+// interface InfoSectionProps {
+//     gid: GameID | undefined
+//     game: ConnectGame | null
+//     player: ConnectPiece | ''
+//     shareMessage: string
+//     shareGame: () => void
+// }
+
+// const InfoSection: React.FC<InfoSectionProps> = ({ gid, game, player, shareMessage, shareGame }) => {
+//     const navigate = useNavigate()
+
+//     return (
+//         <div className='flex flex-col max-w-xl w-full gap-5'>
+//             <div className='py-2 px-3 gap-3 shadow rounded-md grid grid-cols-2'>
+//                 <div className=''>
+//                     <p className='text-sm text-red-500'>{redPlayer(player)}</p>
+//                     <p>{game?.player1Name || 'Loading'}</p>
+//                 </div>
+//                 <div className='text-right'>
+//                     <p className='text-sm text-yellow-500'>{yellowPlayer(player)}</p>
+//                     <p>{game?.player2Name || 'Loading'}</p>
+//                 </div>
+//                 <div className='col-span-2 text-center font-bold'>
+//                     {game === null ? 'LOADING' : connectGameMessage(game, player)}
+//                 </div>
+//             </div>
+//             {game !== null && game.winner !== '' && player !== '' && (
+//                 <Button
+//                     onClick={() => {
+//                         if (gid === undefined || game === null) {
+//                             return
+//                         }
+//                         const uid = player === '1' ? game.player1 : game.player2
+//                         const name = player === '1' ? game.player1Name : game.player2Name
+//                         const opponent = player === '1' ? game.player2 : game.player1
+//                         createConnectGame(uid, name, id => {
+//                             sendGameInvite(opponent, 'connect', id)
+//                             navigate(`/play/connect/${id}`)
+//                         })
+//                     }}
+//                     className='bg-sky-500 text-white'
+//                 >
+//                     Rematch Opponent
+//                 </Button>
+//             )}
+//             <Button
+//                 onClick={shareGame}
+//                 disabled={game === null || game.winner !== ''}
+//                 className='bg-sky-500 text-white disabled:bg-gray-400'
+//             >
+//                 {shareMessage}
+//             </Button>
+//             <span className='-mt-3 text-sm text-gray-600'>
+//                 Invite code: {window.location.pathname.split('/').at(-1)}
+//             </span>
+//         </div>
+//     )
+// }
 
 // Returns the index of the player, else -1
 const getPlayerIndex = (game: YahtzeeGame, uid: UserID): number => {
@@ -247,4 +327,35 @@ const getPlayerIndex = (game: YahtzeeGame, uid: UserID): number => {
         }
     }
     return -1
+}
+
+type Die = {
+    value: DieV
+    locked: boolean
+}
+type DieV = 1 | 2 | 3 | 4 | 5 | 6
+
+const gameDiceToPlayDice = (dice: number[]): Die[] => dice.map(value => ({ value: numberToDieV(value), locked: false }))
+
+const numberToDieV = (die: number): DieV => {
+    switch (die) {
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 3
+        case 4:
+            return 4
+        case 5:
+            return 5
+        default:
+            return 6
+    }
+}
+
+const rollRandomDie = (): DieV => numberToDieV(Math.floor(Math.random() * 6) + 1)
+
+const rollRandomDice = (dice: Die[]): Die[] => {
+    return dice.map(({ value, locked }) => (locked ? { value, locked } : { value: rollRandomDie(), locked }))
 }
