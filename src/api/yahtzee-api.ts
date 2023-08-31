b1133d85e9787b20bd01e694bc9acfc7c2ce38d5
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-09-01 00:50:51 +1000

    Add rolls and moves for yahtzee

--- src/api/yahtzee-api.ts
+++ src/api/yahtzee-api.ts
@@ -20,6 +20,22 @@ export type YahtzeePlayer = {
 
 export type CardValue = number | null
 
+export type YahtzeeMove =
+    | 'ones'
+    | 'twos'
+    | 'threes'
+    | 'fours'
+    | 'fives'
+    | 'sixes'
+    | 'triple'
+    | 'quadruple'
+    | 'fullHouse'
+    | 'smallStraight'
+    | 'largeStraight'
+    | 'yahtzee'
+    | 'extras'
+    | 'chance'
+
 export type YahtzeeCard = {
     ones: CardValue
     twos: CardValue
@@ -67,9 +83,9 @@ export const createYahtzeeGame = (uid: UserID, name: string, callback: (id: Game
         players: [newPlayer(uid, name)],
         playersJoined: false,
         turn: {
-            playerIndex: -1,
+            playerIndex: 0,
             roll: 0,
-            dice: [1, 1, 1, 1, 1],
+            dice: randomDice([1, 1, 1, 1, 1], []),
         },
         winner: -1,
     }
@@ -130,4 +146,193 @@ export const putYahtzeeAllPlayersJoined = (gid: GameID) => {
         const errorMessage = error.message
         console.error('[YAHTZEE] Setting playersJoined failed with code', errorCode, errorMessage)
     })
+}
+
+const getPlayerIndex = (game: YahtzeeGame, uid: UserID): number => {
+    for (let i = 0; i < game.players.length; i++) {
+        if (game.players[i].uid === uid) {
+            return i
+        }
+    }
+    return -1
+}
+
+const randomDice = (dice: number[], keepIndex: number[]): number[] => {
+    for (let i = 0; i < 5; i++) {
+        if (!keepIndex.includes(i)) {
+            dice[i] = Math.floor(Math.random() * 6) + 1
+        }
+    }
+    return dice
+}
+
+export const postYahtzeeRoll = (gid: GameID, uid: UserID, keepIndex: number[]) => {
+    const gameRef = doc(database, 'yahtzee', gid)
+
+    try {
+        runTransaction(database, async transaction => {
+            const doc = await transaction.get(gameRef)
+            if (doc.exists()) {
+                const data = doc.data()
+                const game: YahtzeeGame = {
+                    players: data.players,
+                    playersJoined: data.playersJoined,
+                    turn: data.turn,
+                    winner: data.winner,
+                }
+                const playerIndex = getPlayerIndex(game, uid)
+                if (
+                    !game.playersJoined ||
+                    playerIndex === -1 ||
+                    playerIndex !== game.turn.playerIndex ||
+                    game.turn.roll >= 3
+                ) {
+                    return
+                }
+                if (game.turn.roll === 0) {
+                    keepIndex = []
+                }
+                transaction.update(gameRef, {
+                    turn: {
+                        playerIndex,
+                        roll: game.turn.roll + 1,
+                        dice: randomDice(game.turn.dice, keepIndex),
+                    },
+                })
+            }
+        })
+    } catch (e) {
+        console.error('[YAHTZEE] Rolling dice failed', e)
+    }
+}
+
+const sum = (numbers: number[]): number => numbers.reduce((s, v) => s + v, 0)
+const moveMap = (move: YahtzeeMove, card: YahtzeeCard, dice: number[]): { existing: CardValue; worth: number } => {
+    switch (move) {
+        case 'ones':
+            return { existing: card.ones, worth: sum(dice.filter(v => v === 1)) }
+        case 'twos':
+            return { existing: card.twos, worth: sum(dice.filter(v => v === 2)) }
+        case 'threes':
+            return { existing: card.threes, worth: sum(dice.filter(v => v === 3)) }
+        case 'fours':
+            return { existing: card.fours, worth: sum(dice.filter(v => v === 4)) }
+        case 'fives':
+            return { existing: card.fives, worth: sum(dice.filter(v => v === 5)) }
+        case 'sixes':
+            return { existing: card.sixes, worth: sum(dice.filter(v => v === 6)) }
+        case 'triple':
+            return { existing: card.triple, worth: sum(dice) }
+        case 'quadruple':
+            return { existing: card.quadruple, worth: sum(dice) }
+        case 'fullHouse':
+            return { existing: card.fullHouse, worth: 25 }
+        case 'smallStraight':
+            return { existing: card.smallStraight, worth: 30 }
+        case 'largeStraight':
+            return { existing: card.largeStraight, worth: 40 }
+        case 'yahtzee':
+            return { existing: card.yahtzee, worth: 50 }
+        case 'extras':
+            return { existing: card.extras, worth: 100 }
+        case 'chance':
+            return { existing: card.chance, worth: sum(dice) }
+    }
+}
+
+export const postYahtzeeMove = (gid: GameID, uid: UserID, option: YahtzeeMove, scratch: boolean) => {
+    const gameRef = doc(database, 'yahtzee', gid)
+
+    try {
+        runTransaction(database, async transaction => {
+            const doc = await transaction.get(gameRef)
+            if (doc.exists()) {
+                const data = doc.data()
+                const game: YahtzeeGame = {
+                    players: data.players,
+                    playersJoined: data.playersJoined,
+                    turn: data.turn,
+                    winner: data.winner,
+                }
+                const playerIndex = getPlayerIndex(game, uid)
+                if (
+                    !game.playersJoined ||
+                    playerIndex === -1 ||
+                    playerIndex !== game.turn.playerIndex ||
+                    game.turn.roll === 0 ||
+                    game.turn.roll > 3
+                ) {
+                    return
+                }
+                const { existing, worth } = moveMap(option, game.players[playerIndex].card, game.turn.dice)
+                if (option === 'extras') {
+                    game.players[playerIndex].card.extras = (game.players[playerIndex].card.extras || 0) + worth
+                    if (game.players[playerIndex].card.ones === null && game.turn.dice[0] === 1) {
+                        game.players[playerIndex].card.ones = 5
+                    } else if (game.players[playerIndex].card.twos === null && game.turn.dice[0] === 2) {
+                        game.players[playerIndex].card.twos = 10
+                    } else if (game.players[playerIndex].card.threes === null && game.turn.dice[0] === 3) {
+                        game.players[playerIndex].card.threes = 15
+                    } else if (game.players[playerIndex].card.fours === null && game.turn.dice[0] === 4) {
+                        game.players[playerIndex].card.fours = 20
+                    } else if (game.players[playerIndex].card.fives === null && game.turn.dice[0] === 5) {
+                        game.players[playerIndex].card.fives = 25
+                    } else if (game.players[playerIndex].card.sixes === null && game.turn.dice[0] === 6) {
+                        game.players[playerIndex].card.sixes = 30
+                    } else if (game.players[playerIndex].card.triple === null) {
+                        game.players[playerIndex].card.triple = sum(game.turn.dice)
+                    } else if (game.players[playerIndex].card.quadruple === null) {
+                        game.players[playerIndex].card.quadruple = sum(game.turn.dice)
+                    } else if (game.players[playerIndex].card.fullHouse === null) {
+                        game.players[playerIndex].card.fullHouse = 25
+                    } else if (game.players[playerIndex].card.smallStraight === null) {
+                        game.players[playerIndex].card.smallStraight = 30
+                    } else if (game.players[playerIndex].card.largeStraight === null) {
+                        game.players[playerIndex].card.largeStraight = 40
+                    } else if (game.players[playerIndex].card.chance === null) {
+                        game.players[playerIndex].card.chance = sum(game.turn.dice)
+                    }
+                } else if (existing !== null) {
+                    return
+                } else if (option === 'ones') {
+                    game.players[playerIndex].card.ones = scratch ? 0 : worth
+                } else if (option === 'twos') {
+                    game.players[playerIndex].card.twos = scratch ? 0 : worth
+                } else if (option === 'threes') {
+                    game.players[playerIndex].card.threes = scratch ? 0 : worth
+                } else if (option === 'fours') {
+                    game.players[playerIndex].card.fours = scratch ? 0 : worth
+                } else if (option === 'fives') {
+                    game.players[playerIndex].card.fives = scratch ? 0 : worth
+                } else if (option === 'sixes') {
+                    game.players[playerIndex].card.sixes = scratch ? 0 : worth
+                } else if (option === 'triple') {
+                    game.players[playerIndex].card.triple = scratch ? 0 : worth
+                } else if (option === 'quadruple') {
+                    game.players[playerIndex].card.quadruple = scratch ? 0 : worth
+                } else if (option === 'fullHouse') {
+                    game.players[playerIndex].card.fullHouse = scratch ? 0 : worth
+                } else if (option === 'smallStraight') {
+                    game.players[playerIndex].card.smallStraight = scratch ? 0 : worth
+                } else if (option === 'largeStraight') {
+                    game.players[playerIndex].card.largeStraight = scratch ? 0 : worth
+                } else if (option === 'yahtzee') {
+                    game.players[playerIndex].card.yahtzee = scratch ? 0 : worth
+                } else if (option === 'chance') {
+                    game.players[playerIndex].card.chance = scratch ? 0 : worth
+                }
+                console.log('[YAHTZEE] Updating card to', game.players[playerIndex].card)
+                transaction.update(gameRef, {
+                    players: game.players,
+                    turn: {
+                        playerIndex: (playerIndex + 1) % game.players.length,
+                        roll: 0,
+                        dice: game.turn.dice,
+                    },
+                })
+            }
+        })
+    } catch (e) {
+        console.error('[YAHTZEE] Making move failed', e)
+    }
 }
